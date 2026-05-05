83c6cd67b253a0a5c7f7f430511200c387d5791e
Georg Nebehay <gnebehay@gmail.com>
2026-05-05 19:53:55 +0200

    Update sample config file

--- wette/config-sqlite-sample.py
+++ wette/config-sqlite-sample.py
@@ -14,7 +14,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 PASSWORD_SALT = 'my-uber-secred-salt'
 
-ADMIN_MAIL = 'abc@def.gh'
+ADMIN_MAILS= ['abc@def.gh']
 
 # This is the sender address of mails
 MAIN_MAIL = 'abc@def.gh'
@@ -22,9 +22,3 @@ MAIN_MAIL = 'abc@def.gh'
 MSG_NOT_PAID = "You have not paid yet. Please contact <a href='mailto:{MAIN_MAIL}'>{MAIN_MAIL}</a> for payment options. If you don't pay until the beginning of the first match, you will be scratched.".format(MAIN_MAIL=MAIN_MAIL)
 
 SECRET_KEY = 'abc'
-
-# Flask-User settings
-USER_APP_NAME = "Flask-User QuickStart App"      # Shown in and email templates and page footers
-USER_ENABLE_EMAIL = False      # Disable email authentication
-USER_ENABLE_USERNAME = True    # Enable username authentication
-USER_REQUIRE_RETYPE_PASSWORD = False    # Simplify register form
